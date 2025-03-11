c8867cc2725678fbd8ca3f75811907d12bebd443
angelicapham <112834575+angelicapham@users.noreply.github.com>
2025-03-10 22:48:36 -0400

    fixed suite implementation

--- models/suite.py
+++ models/suite.py
@@ -1,12 +1,11 @@
 from models.base import Base
+from models.resco import ResidentialCollege
 
 from sqlalchemy import (
     ForeignKey,
     Integer, String
 )
-from sqlalchemy.orm import Mapped, mapped_column, relationship
-
-from models.resco import ResidentialCollege
+from sqlalchemy.orm import backref, Mapped, mapped_column, relationship
 
 class Suite(Base):
     __tablename__ = "suites"
@@ -14,8 +13,12 @@ class Suite(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(String)
     entryway: Mapped[str] = mapped_column(String)
+
     capacity: Mapped[int] = mapped_column(Integer)
+    singles: Mapped[int] = mapped_column(Integer)
+    doubles: Mapped[int] = mapped_column(Integer)
+
     year: Mapped[int] = mapped_column(Integer)
 
     resco_id: Mapped[int] = mapped_column(Integer, ForeignKey('rescos.id'))
-    resco: Mapped[ResidentialCollege] = relationship("ResidentialCollege")
+    resco: Mapped["ResidentialCollege"] = relationship("ResidentialCollege", backref=backref("suite"))
