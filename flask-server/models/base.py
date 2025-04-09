ed3099cc805d87f71fc6923823e11d3af756fc62
Brandon Nguyen <112731698+bxngyn@users.noreply.github.com>
2025-04-09 15:57:32 -0400

    temporary remove react ver. for alpha submission

--- flask-server/models/base.py
+++
@@ -1,7 +0,0 @@
-
-from sqlalchemy.orm import DeclarativeBase
-
-class Base(DeclarativeBase):
-    """Represents fixture of object-relational mapping system to bridge
-    object-oriented programming and relational database model"""
-    pass
