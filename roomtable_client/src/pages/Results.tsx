be1892e7dbe5dc5f234677bbfaf2bfc67cb1dbb6
mwz773 <112724619+mwz773@users.noreply.github.com>
2025-04-15 15:19:41 -0400

    fixed formatting for results

--- roomtable_client/src/pages/Results.tsx
+++ roomtable_client/src/pages/Results.tsx
@@ -43,24 +43,30 @@ const ResultsPage = () => {
 
       <h2>Matching Suites</h2>
       {results.length > 0 ? (
-        <table>
-          <thead>
+        <table className="min-w-full border border-gray-300 shadow-sm rounded-md overflow-hidden">
+          <thead className="bg - [#00356B] text-white uppercase text-sm">
             <tr>
-              <th>Suite Name</th>
-              <th>Entryway</th>
-              <th>Capacity</th>
-              <th>Residential College</th>
+              <th className="px-4 py-2 text-left ">Suite Name</th>
+              <th className="px-4 py-2 text-left ">Entryway</th>
+              <th className="px-4 py-2 text-left ">Capacity</th>
+              <th className="px-4 py-2 text-left ">Residential College</th>
             </tr>
           </thead>
           <tbody>
-            {results.map((suite) => (
-              <tr key={suite.id}>
-                <td>
-                  <a href={`/summary/${suite.id}`}>{suite.name}</a>
+            {results.map((suite, index) => (
+              <tr 
+              key={suite.id}
+              className={index % 2 === 0? "bg-gray 100" : "bg-gray-200"}>
+                <td className = "px-4 py-2">
+                  <a href={`/summary/${suite.id}`}
+                  className = "text-blue-700 underline hover:text-blue-9000"
+                  >
+                    {suite.name}
+                    </a>
                 </td>
-                <td>{suite.entryway}</td>
-                <td>{suite.capacity}</td>
-                <td>{suite.resco?.name}</td>
+                <td className="px-4 py-2" >{suite.entryway}</td>
+                <td className="px-4 py-2">{suite.capacity}</td>
+                <td className="px-4 py-2">{suite.resco?.name}</td>
               </tr>
             ))}
           </tbody>
@@ -69,7 +75,8 @@ const ResultsPage = () => {
         <p>No suites match the search criteria.</p>
       )}
 
-      <button onClick={() => navigate("/search")} className="nav-button">
+      <button onClick={() => navigate("/search")}   className="mt-6 bg-[#00356B] hover:bg-[#00509E] text-white font-bold py-2 px-6 rounded shadow-md transition-all"
+      >
         Back to Search
       </button>
     </div>
