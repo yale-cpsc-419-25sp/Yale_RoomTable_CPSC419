3fd98d3d517c94594f5b32444bcab94154a1b0be
mwz773 <112724619+mwz773@users.noreply.github.com>
2025-04-16 12:48:37 -0400

    added logo to navbar

--- roomtable_client/src/components/Navbar.tsx
+++ roomtable_client/src/components/Navbar.tsx
@@ -4,7 +4,15 @@ import UserDropdown from './Dropdown';
 const Navbar = ({ user }) => {
     return (
         <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4'>
-            <h1 className='w-full text-3xl font-bold'>RoomTable</h1>
+            <div className="flex items-center space-x-2">
+                <h1 className="text-3xl font-bold">RoomTable</h1>
+                <img
+                    src="/floorplans/logowithoutroomtable.jpg"
+                    alt="RoomTable Logo"
+                    className="h-16 w-12 object-contain -mt-2"
+                />
+                
+            </div>
             <ul className='flex'>
                 {!user ? (
                     <>
