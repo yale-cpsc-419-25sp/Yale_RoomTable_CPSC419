89b3de4a6f5b8ddf105817e2543c03377a742358
Briley Thomas <112785736+brileythomas1@users.noreply.github.com>
2025-04-18 16:25:37 -0400

    Remove pre-React version

--- roomtable_client/src/pages/Friends.tsx
+++ roomtable_client/src/pages/Friends.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "../../../static/search.css"
+import "../search.css"
 
 export default function Friends() {
   const [friends, setFriends] = useState([]);
